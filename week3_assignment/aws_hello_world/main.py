7de59db6fc360f277d9f88dc982dc9625d433e89
Veer S <vsummanwar@gmail.com>
2022-04-30 14:21:50 +0530

    fastapi added

--- week3_assignment/aws_hello_world/main.py
+++ week3_assignment/aws_hello_world/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
-print("hello AWS world!")
 app = FastAPI()
 
-@app.get("/health")
-def health():
-    return "hello google"
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
